f23b2e4eb5e3a44ff0991954dc00ad2ff20664de
棋轩 <fanlb@ibuscloud.com>
2021-05-17 09:15:53 +0800

    feat: promise

---
+++ practice/test.ts
@@ -0,0 +1,143 @@
+//重写区，用于自己测试自己从0再写一次的能力
+class myPromise {
+  state = "pending";
+  taskList = [];
+  value = null;
+  failReason = null;
+  constructor(fn) {
+    try {
+      fn(this._resolve.bind(this), this._reject.bind(this));
+    } catch (err) {
+      this._reject(err);
+    }
+  }
+  then(onFulfilled, onReject) {
+    return new myPromise((resolve, reject) => {
+      this._handle({
+        onFulfilled,
+        onReject,
+        resolve,
+        reject,
+      });
+    });
+  }
+  _handle({ onFulfilled, onReject, resolve, reject }) {
+    if (this.state === "pending") {
+      //注入then和catch时暂存任务
+      this.taskList.push({ onFulfilled, onReject, resolve, reject });
+    } else {
+      const runFn = this.state === "fulfilled" ? onFulfilled : onReject;
+      const inputValue =
+        this.state === "fulfilled" ? this.value : this.failReason;
+      if (!runFn) {
+        (this.state === "fulfilled" ? resolve : resolve)(inputValue);
+        return;
+      } else {
+        try {
+          const value = runFn(inputValue);
+          resolve(value);
+        } catch (err) {
+          reject(err);
+        }
+      }
+    }
+  }
+  catch(onReject) {
+    this.then.call(this, null, onReject);
+  }
+  _resolve(value) {
+    if (this.state !== "pending") return;
+    if (value && value.then === "function") {
+      value.then.call(this, this._resolve.bind(this), this._reject.bind(this));
+      return;
+    }
+
+    //延迟执行，方便即时执行的resolve注入then和catch
+    setTimeout(() => {
+      this.value=  value;
+      this.state = 'fulfilled';
+      this.taskList.forEach((ele) => this._handle(ele));
+    });
+  }
+  _reject(error) {
+    if (this.state !== "pending") return;
+    //延迟执行，方便即时执行的resolve注入then和catch
+    setTimeout(() => {
+      this.value=  error;
+      this.state = 'rejected';
+      this.taskList.forEach((ele) => this._handle(ele));
+    });
+  }
+}
+
+/* class myPromise {
+  state = "pending";
+  taskList = [];
+  value = null;
+  reason = null;
+  constructor(fn) {
+    try {
+      fn(this._resolve.bind(this), this._reject.bind(this));
+    } catch (error) {
+      this._reject(error);
+    }
+  }
+  then(onFulfilled, onReject) {
+    return new myPromise((resolve, reject) => {
+      this._handle({
+        onFulfilled,
+        onReject,
+        resolve,
+        reject,
+      });
+    });
+  }
+  catch(onReject) {
+    this.then.call(this, null, onReject);
+  }
+  _handle({ onFulfilled, onReject, resolve, reject }) {
+    if (this.state === "pending") {
+      //如果pending，暂存任务
+      this.taskList.push({ onFulfilled, onReject, resolve, reject });
+    } else {
+      //否则说明是其他情况进来的，直接内置promise的执行函数
+      const runFn = this.state === "fulfilled" ? onFulfilled : onReject;
+      const inputValue = this.state === "fulfilled" ? this.value : this.reason;
+      if (!runFn) {
+        //如果没有传入需要run的function
+        (this.state === "fulfilled" ? resolve : reject)(inputValue);
+        return;
+      }
+      try {
+        const value = runFn(inputValue);
+        resolve(value);
+      } catch (err) {
+        reject(err);
+      }
+    }
+  }
+  _resolve(value) {
+    if (this.state !== "pending") return;
+
+    if (value && value.then === "function") {
+      const valueThen = value.then;
+      valueThen.call(this, this._resolve.bind(this), this._reject.bind(this));
+      return;
+    }
+
+    setTimeout(() => {
+      this.value = value;
+      this.state = "fulfilled";
+      this.taskList.forEach((ele) => this._handle(ele));
+    });
+  }
+  _reject(err) {
+    if (this.state !== "pending") return;
+    setTimeout(() => {
+      this.reason = err;
+      this.state = "rejected";
+      this.taskList.forEach((ele) => this._handle(ele));
+    });
+  }
+}
+ */
