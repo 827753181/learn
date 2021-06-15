2f2758bc0036cdc996c5c81a93561b6f9de4c6a4
fanlinbo <827753181@qq.com>
2021-06-15 17:42:23 +0800

    feat: base viteProject for vue lib

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import { defineConfig } from "vite";
+import reactRefresh from "@vitejs/plugin-react-refresh";
+import legacy from "@vitejs/plugin-legacy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
-})
+  plugins: [
+    reactRefresh(),
+    process.env.NODE_ENV === "production"
+      ? legacy({
+          targets: ["ie >= 11"],
+          additionalLegacyPolyfills: ["regenerator-runtime/runtime"],
+        })
+      : undefined,
+  ],
+});
