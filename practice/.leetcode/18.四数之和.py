13118629a4556838bf2476533181076e8ee62aeb
fanlinbo <827753181@qq.com>
2022-03-08 18:07:02 +0800

    feat: today

--- practice/.leetcode/18.四数之和.py
+++ practice/.leetcode/18.四数之和.py
@@ -55,7 +55,43 @@
 # @lc code=start
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        pass
+        arr = list()
+        if not nums or len(nums) < 4:
+            return arr
+
+        nums.sort()
+        length = len(nums)
+        for i in range(length - 3):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            if nums[i]+nums[i+1]+nums[i+2]+nums[i+3] > target:
+                break
+
+            if nums[i]+nums[length-1]+nums[length-2]+nums[length-3] < target:
+                continue
+            for j in range(i+1, length-2):
+                if j > i+1 and nums[j] == nums[j-1]:
+                    continue
+                if nums[i]+nums[j]+nums[j+1]+nums[j+2] > target:
+                    break
+                if nums[i]+nums[j]+nums[length-1]+nums[length-2] < target:
+                    continue
+                left, right = j+1, length-1
+                while left < right:
+                    total = nums[i]+nums[j]+nums[left]+nums[right]
+                    if total < target:
+                        left += 1
+                    elif total > target:
+                        right -= 1
+                    else:
+                        arr.append([nums[i], nums[j], nums[left], nums[right]])
+                        while left < right and nums[left] == nums[left+1]:
+                            left += 1
+                        left += 1
+                        while left < right and nums[right] == nums[right-1]:
+                            right -= 1
+                        right -= 1
+        return arr
 # @lc code=end
 
 # class Solution:
--- practice/.leetcode/19.删除链表的倒数第-n-个结点.py
+++ practice/.leetcode/19.删除链表的倒数第-n-个结点.py
@@ -14,56 +14,67 @@
 # Testcase Example:  '[1,2,3,4,5]\n2'
 #
 # 给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
-# 
-# 
-# 
+#
+#
+#
 # 示例 1：
-# 
-# 
+#
+#
 # 输入：head = [1,2,3,4,5], n = 2
 # 输出：[1,2,3,5]
-# 
-# 
+#
+#
 # 示例 2：
-# 
-# 
+#
+#
 # 输入：head = [1], n = 1
 # 输出：[]
-# 
-# 
+#
+#
 # 示例 3：
-# 
-# 
+#
+#
 # 输入：head = [1,2], n = 1
 # 输出：[1]
-# 
-# 
-# 
-# 
+#
+#
+#
+#
 # 提示：
-# 
-# 
+#
+#
 # 链表中结点的数目为 sz
 # 1 <= sz <= 30
 # 0 <= Node.val <= 100
 # 1 <= n <= sz
-# 
-# 
-# 
-# 
+#
+#
+#
+#
 # 进阶：你能尝试使用一趟扫描实现吗？
-# 
+#
 #
 
 # @lc code=start
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
 class Solution:
     def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
-        pass
+        headS = ListNode(0, head)
+        cur = head
+        pre = headS
+        for i in range(n):
+            cur = cur.next
+        while cur:
+            cur = cur.next
+            pre = pre.next
+        pre.next = pre.next.next
+        return headS.next
 # @lc code=end
 
 # class Solution:
@@ -74,7 +85,7 @@ class Solution:
 #                 length += 1
 #                 head = head.next
 #             return length
-        
+
 #         dummy = ListNode(0, head)
 #         length = getLength(head)
 #         cur = dummy
@@ -82,7 +93,6 @@ class Solution:
 #             cur = cur.next
 #         cur.next = cur.next.next
 #         return dummy.next
-
 
 
 # class Solution:
@@ -96,6 +106,6 @@ class Solution:
 #         while first:
 #             first = first.next
 #             second = second.next
-        
+
 #         second.next = second.next.next
 #         return dummy.next
