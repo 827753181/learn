ef38505fab4b2a38e26c80f7bbbc8f5f4d31034b
fanlinbo <827753181@qq.com>
2022-03-10 18:20:44 +0800

    feat: today

--- practice/.leetcode/22.括号生成.py
+++ practice/.leetcode/22.括号生成.py
@@ -14,40 +14,52 @@
 # Testcase Example:  '3'
 #
 # 数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
-# 
-# 
-# 
+#
+#
+#
 # 示例 1：
-# 
-# 
+#
+#
 # 输入：n = 3
 # 输出：["((()))","(()())","(())()","()(())","()()()"]
-# 
-# 
+#
+#
 # 示例 2：
-# 
-# 
+#
+#
 # 输入：n = 1
 # 输出：["()"]
-# 
-# 
-# 
-# 
+#
+#
+#
+#
 # 提示：
-# 
-# 
+#
+#
 # 1 <= n <= 8
-# 
-# 
+#
+#
 #
 
 # @lc code=start
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        pass
+        ans = []
+        def backtrace(arr, left, right):
+            if len(arr) == n*2:
+                ans.append(''.join(arr))
+            else:
+                if(left < n):
+                    arr.append('(')
+                    backtrace(arr, left+1, right)
+                    arr.pop()
+                if(right < left):
+                    arr.append(')')
+                    backtrace(arr, left, right+1)
+                    arr.pop()
+        backtrace([], 0, 0)
+        return ans
 # @lc code=end
-
-
 
 
 # class Solution:
@@ -77,8 +89,6 @@ class Solution:
 #         return ans
 
 
-
-
 # class Solution:
 #     def generateParenthesis(self, n: int) -> List[str]:
 #         ans = []
@@ -97,7 +107,6 @@ class Solution:
 
 #         backtrack([], 0, 0)
 #         return ans
-
 
 
 # class Solution:
