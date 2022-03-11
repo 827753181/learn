6cd7a4410efe059496d986bb3add72b30029d71f
fanlinbo <827753181@qq.com>
2022-03-11 18:30:24 +0800

    feat: solution

---
+++ practice/.leetcode/34.在排序数组中查找元素的第一个和最后一个位置.py
@@ -0,0 +1,90 @@
+#
+# @lc app=leetcode.cn id=34 lang=python3
+#
+# [34] 在排序数组中查找元素的第一个和最后一个位置
+#
+# https://leetcode-cn.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
+#
+# algorithms
+# Medium (42.31%)
+# Likes:    1518
+# Dislikes: 0
+# Total Accepted:    461.6K
+# Total Submissions: 1.1M
+# Testcase Example:  '[5,7,7,8,8,10]\n8'
+#
+# 给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
+# 
+# 如果数组中不存在目标值 target，返回 [-1, -1]。
+# 
+# 进阶：
+# 
+# 
+# 你可以设计并实现时间复杂度为 O(log n) 的算法解决此问题吗？
+# 
+# 
+# 
+# 
+# 示例 1：
+# 
+# 
+# 输入：nums = [5,7,7,8,8,10], target = 8
+# 输出：[3,4]
+# 
+# 示例 2：
+# 
+# 
+# 输入：nums = [5,7,7,8,8,10], target = 6
+# 输出：[-1,-1]
+# 
+# 示例 3：
+# 
+# 
+# 输入：nums = [], target = 0
+# 输出：[-1,-1]
+# 
+# 
+# 
+# 提示：
+# 
+# 
+# 0 
+# -10^9 
+# nums 是一个非递减数组
+# -10^9 
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        pass
+# @lc code=end
+
+
+
+
+# const binarySearch = (nums, target, lower) => {
+#     let left = 0, right = nums.length - 1, ans = nums.length;
+#     while (left <= right) {
+#         const mid = Math.floor((left + right) / 2);
+#         if (nums[mid] > target || (lower && nums[mid] >= target)) {
+#             right = mid - 1;
+#             ans = mid;
+#         } else {
+#             left = mid + 1;
+#         }
+#     }
+#     return ans;
+# }
+
+# var searchRange = function(nums, target) {
+#     let ans = [-1, -1];
+#     const leftIdx = binarySearch(nums, target, true);
+#     const rightIdx = binarySearch(nums, target, false) - 1;
+#     if (leftIdx <= rightIdx && rightIdx < nums.length && nums[leftIdx] === target && nums[rightIdx] === target) {
+#         ans = [leftIdx, rightIdx];
+#     } 
+#     return ans;
+# };
---
+++ practice/.leetcode/40.组合总和-ii.py
@@ -0,0 +1,120 @@
+#
+# @lc app=leetcode.cn id=40 lang=python3
+#
+# [40] 组合总和 II
+#
+# https://leetcode-cn.com/problems/combination-sum-ii/description/
+#
+# algorithms
+# Medium (61.26%)
+# Likes:    868
+# Dislikes: 0
+# Total Accepted:    252.8K
+# Total Submissions: 413.5K
+# Testcase Example:  '[10,1,2,7,6,1,5]\n8'
+#
+# 给定一个候选人编号的集合 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。
+# 
+# candidates 中的每个数字在每个组合中只能使用 一次 。
+# 
+# 注意：解集不能包含重复的组合。 
+# 
+# 
+# 
+# 示例 1:
+# 
+# 
+# 输入: candidates = [10,1,2,7,6,1,5], target = 8,
+# 输出:
+# [
+# [1,1,6],
+# [1,2,5],
+# [1,7],
+# [2,6]
+# ]
+# 
+# 示例 2:
+# 
+# 
+# 输入: candidates = [2,5,2,1,2], target = 5,
+# 输出:
+# [
+# [1,2,2],
+# [5]
+# ]
+# 
+# 
+# 
+# 提示:
+# 
+# 
+# 1 <= candidates.length <= 100
+# 1 <= candidates[i] <= 50
+# 1 <= target <= 30
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        pass
+# @lc code=end
+
+
+
+# class Solution:
+#     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+#         def dfs(pos: int, rest: int):
+#             nonlocal sequence
+#             if rest == 0:
+#                 ans.append(sequence[:])
+#                 return
+#             if pos == len(freq) or rest < freq[pos][0]:
+#                 return
+            
+#             dfs(pos + 1, rest)
+
+#             most = min(rest // freq[pos][0], freq[pos][1])
+#             for i in range(1, most + 1):
+#                 sequence.append(freq[pos][0])
+#                 dfs(pos + 1, rest - i * freq[pos][0])
+#             sequence = sequence[:-most]
+        
+#         freq = sorted(collections.Counter(candidates).items())
+#         ans = list()
+#         sequence = list()
+#         dfs(0, target)
+#         return ans
+
+
+from typing import List
+
+
+# class Solution:
+
+#     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+#         def dfs(begin, path, residue):
+#             if residue == 0:
+#                 res.append(path[:])
+#                 return
+
+#             for index in range(begin, size):
+#                 if candidates[index] > residue:
+#                     break
+
+#                 if index > begin and candidates[index - 1] == candidates[index]:
+#                     continue
+
+#                 path.append(candidates[index])
+#                 dfs(index + 1, path, residue - candidates[index])
+#                 path.pop()
+
+#         size = len(candidates)
+#         if size == 0:
+#             return []
+
+#         candidates.sort()
+#         res = []
+#         dfs(0, [], target)
+#         return res
