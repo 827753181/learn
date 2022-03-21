14d0abd4acf521b6fb2e02b89324ad1395834e77
fanlinbo <Linbo.Fan@geely.com>
2022-03-21 10:37:13 +0800

    feat: 去除无用包

--- sourceCode/nest/nest-master/integration/hello-world/src/app.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { HelloModule } from './hello/hello.module';
-import { HostModule } from './host/host.module';
-
-@Module({
-  imports: [HelloModule, HostModule],
-})
-export class ApplicationModule {}
