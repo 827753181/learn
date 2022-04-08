14f6acd42133d53b62ad752940c5f932c0deedcf
fanlinbo <827753181@qq.com>
2022-04-08 16:06:10 +0800

    feat: yestoday

--- practice/.leetcode/57.插入区间.py
+++ practice/.leetcode/57.插入区间.py
@@ -14,65 +14,96 @@
 # Testcase Example:  '[[1,3],[6,9]]\n[2,5]'
 #
 # 给你一个 无重叠的 ，按照区间起始端点排序的区间列表。
-# 
+#
 # 在列表中插入一个新的区间，你需要确保列表中的区间仍然有序且不重叠（如果有必要的话，可以合并区间）。
-# 
-# 
-# 
+#
+#
+#
 # 示例 1：
-# 
-# 
+#
+#
 # 输入：intervals = [[1,3],[6,9]], newInterval = [2,5]
 # 输出：[[1,5],[6,9]]
-# 
-# 
+#
+#
 # 示例 2：
-# 
-# 
+#
+#
 # 输入：intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
 # 输出：[[1,2],[3,10],[12,16]]
 # 解释：这是因为新的区间 [4,8] 与 [3,5],[6,7],[8,10] 重叠。
-# 
+#
 # 示例 3：
-# 
-# 
+#
+#
 # 输入：intervals = [], newInterval = [5,7]
 # 输出：[[5,7]]
-# 
-# 
+#
+#
 # 示例 4：
-# 
-# 
+#
+#
 # 输入：intervals = [[1,5]], newInterval = [2,3]
 # 输出：[[1,5]]
-# 
-# 
+#
+#
 # 示例 5：
-# 
-# 
+#
+#
 # 输入：intervals = [[1,5]], newInterval = [2,7]
 # 输出：[[1,7]]
-# 
-# 
-# 
-# 
+#
+#
+#
+#
 # 提示：
-# 
-# 
-# 0 
+#
+#
+# 0
 # intervals[i].length == 2
-# 0 
+# 0
 # intervals 根据 intervals[i][0] 按 升序 排列
 # newInterval.length == 2
-# 0 
-# 
-# 
+# 0
+#
+#
 #
 
 # @lc code=start
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        pass
+
+        left, right = newInterval
+        rightAns = [i for i in intervals if i[0] > right]
+        leftAns = [i for i in intervals if i[1] < left]
+        if len(leftAns)+len(rightAns) != len(intervals):
+            left = min(intervals[len(leftAns)][0], left)
+            right = max(intervals[-len(rightAns)-1][1], right)
+        return leftAns+[[left, right]]+rightAns
+        
+        
+        
+        
+        # ans = []
+        # left, right = newInterval
+        # placed = False
+        # for le, ri in intervals:
+        #     if le > right:
+        #         # 左侧在插入点右侧无交集
+        #         if not placed:
+        #             ans.append([left, right])
+        #             placed = True
+        #         ans.append([le, ri])
+        #     elif ri < left:
+        #         # 右侧在插入点左侧无交集
+        #         ans.append([le, ri])
+        #     else:
+        #         # 有交集，插入点变成并集
+        #         left = min(le,left)
+        #         right = max(ri,right)
+        # if not placed:
+        #     ans.append([left,right])
+        # return ans
 # @lc code=end
 
 # class Solution:
@@ -94,7 +125,7 @@ class Solution:
 #                 # 与插入区间有交集，计算它们的并集
 #                 left = min(left, li)
 #                 right = max(right, ri)
-        
+
 #         if not placed:
 #             ans.append([left, right])
 #         return ans
